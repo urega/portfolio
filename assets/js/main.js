90e590c6a0b315d72cdf86b2bb52360328a43cb2
urega <regarevautama@gmail.com>
2025-02-21 17:38:08 +0700

    second commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,34 @@
 /* =====================================================
    Resume section tabs and tab contents
 ===================================================== */
+const resumeTabs = document.querySelector(".resume-tabs");
+const resumePortfolioTabBtns = resumeTabs.querySelectorAll(".tab-btn");
+const resumeTabContents = document.querySelectorAll(".resume-tab-content");
 
+let resumeTabNav = function (resumeTabClick) {
+  resumeTabContents.forEach((resumeTabContent) => {
+    resumeTabContent.style.display = "none";
+    resumeTabContent.classList.remove("active");
+  });
+
+  resumePortfolioTabBtns.forEach((resumePortfolioTabBtn) => {
+    resumePortfolioTabBtn.classList.remove("active");
+  });
+
+  resumeTabContents[resumeTabClick].style.display = "flex";
+
+  setTimeout(() => {
+    resumeTabContents[resumeTabClick].classList.add("active");
+  }, 100);
+
+  resumePortfolioTabBtns[resumeTabClick].classList.add("active");
+};
+
+resumePortfolioTabBtns.forEach((resumePortfolioTabBtn, i) => {
+  resumePortfolioTabBtn.addEventListener("click", () => {
+    resumeTabNav(i);
+  });
+});
 /* =====================================================
    Service modal open/close function
 ===================================================== */
