5b96bf216fbb55c45dacdcab9f357161d05728da
urega <regarevautama@gmail.com>
2025-03-14 16:03:57 +0700

    add cursor effect

--- assets/js/main.js
+++ assets/js/main.js
@@ -72,44 +72,281 @@ serviceCardWithModals.forEach((serviceCardWithModal) => {
 ===================================================== */
 
 // Filter portfolio cards according to portfolio tabs.
+document.addEventListener("DOMContentLoaded", () => {
+  const portfolioTabs = document.querySelector(".portfolio-tabs");
+  const portfolioTabBtns = portfolioTabs.querySelectorAll(".tab-btn");
+  const cardsWithModals = document.querySelectorAll(
+    ".portfolio-container .card-with-modal"
+  );
+
+  portfolioTabBtns.forEach((tabBtn) => {
+    tabBtn.addEventListener("click", () => {
+      const filter = tabBtn.getAttribute("data-filter");
+
+      cardsWithModals.forEach((cardWithModal) => {
+        if (filter === "all" || cardWithModal.classList.contains(filter)) {
+          cardWithModal.classList.remove("hidden");
+
+          setTimeout(() => {
+            cardWithModal.style.opacity = 1;
+            cardWithModal.style.transition = ".5s ease";
+          }, 150);
+        } else {
+          cardWithModal.classList.add("hidden");
+
+          setTimeout(() => {
+            cardWithModal.style.opacity = 0;
+            cardWithModal.style.transition = ".5s ease";
+          }, 150);
+        }
+      });
+      // add active class to the clicked tab button
+      portfolioTabBtns.forEach((tabBtn) => {
+        tabBtn.classList.remove("active");
+      });
+      tabBtn.classList.add("active");
+    });
+  });
+});
 
 // Open/Close Portfolio modals.
+const portfolioCardsWithModals = document.querySelectorAll(
+  ".portfolio-container .card-with-modal"
+);
+
+portfolioCardsWithModals.forEach((portfolioCardWithModal) => {
+  const portofolioCard =
+    portfolioCardWithModal.querySelector(".portfolio-card");
+  const portfolioBackdrop = portfolioCardWithModal.querySelector(
+    ".portfolio-modal-backdrop"
+  );
+  const portfolioModal =
+    portfolioCardWithModal.querySelector(".portfolio-modal");
+  const modalCloseBtn =
+    portfolioCardWithModal.querySelector(".modal-close-btn");
+
+  portofolioCard.addEventListener("click", () => {
+    portfolioBackdrop.style.display = "flex";
+    setTimeout(() => {
+      portfolioBackdrop.classList.add("active");
+    }, 300);
+
+    setTimeout(() => {
+      portfolioModal.classList.add("active");
+    }, 300);
+  });
+
+  modalCloseBtn.addEventListener("click", () => {
+    setTimeout(() => {
+      portfolioBackdrop.style.display = "none";
+    }, 500);
+    setTimeout(() => {
+      portfolioBackdrop.classList.remove("active");
+      portfolioModal.classList.remove("active");
+    }, 100);
+  });
+});
 
 /* =====================================================
    Testimonial Swiper
 ===================================================== */
 
+var swiper = new Swiper(".rega-client-swiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
 /* =====================================================
    Send/Receive emails from contact form - EmailJS
 ===================================================== */
 
+(function () {
+  // https://dashboard.emailjs.com/admin/account
+  emailjs.init({
+    publicKey: "-C2OG-3dLnTi46x3X",
+  });
+})();
+
+regaContactForm = document.getElementById("rega-contact-form");
+regaContactFromAlert = document.querySelector(".contact-form-alert");
+
+regaContactForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  // these IDs from the previous steps
+  emailjs
+    .sendForm("service_eygcczc", "template_ikeduz7", "#rega-contact-form")
+    .then(
+      () => {
+        //   console.log("SUCCESS!");
+        regaContactFromAlert.innerHTML =
+          "<span> Your Message sent succesfully! <span><i class='ri-checkbox-circle-fill'></i>";
+        regaContactForm.reset();
+
+        setTimeout(() => {
+          regaContactFromAlert.innerHTML = "";
+        }, 5000);
+      },
+      (error) => {
+        //   console.log("FAILED...", error);
+        regaContactFromAlert.innerHTML =
+          "<span>Message not sent! <span><i class='ri-error-warning-fill'></i>";
+        regaContactFromAlert.title = error;
+      }
+    );
+});
+
 /* =====================================================
    Shrink the height of the header on scroll
 ===================================================== */
+window.addEventListener("scroll", () => {
+  const regaHeader = document.querySelector(".rega-header");
+  regaHeader.classList.toggle("shrink", window.scrollY > 0);
+});
 
 /* =====================================================
    Bottom navigation menu
 ===================================================== */
 
 // Each bottom navigation menu items active on page scroll.
+window.addEventListener("scroll", () => {
+  const navMenuSections = document.querySelectorAll(".nav-menu-section");
+  const scrollY = window.pageYOffset;
+  navMenuSections.forEach((navMenuSection) => {
+    let sectionHeight = navMenuSection.offsetHeight;
+    let sectionTop = navMenuSection.offsetTop - 50;
+    let id = navMenuSection.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".bottom-nav .menu li a[href*=" + id + "]")
+        .classList.add("current");
+    } else {
+      document
+        .querySelector(".bottom-nav .menu li a[href*=" + id + "]")
+        .classList.remove("current");
+    }
+  });
+});
 
 // Javascript to show bottom navigation menu on home(page load).
+window.addEventListener("DOMContentLoaded", () => {
+  const bottomNav = document.querySelector(".bottom-nav");
+
+  bottomNav.classList.toggle("active", window.scrollY < 10);
+});
 
 // Javascript to show/hide bottom navigation menu on home(scroll).
+const bottomNav = document.querySelector(".bottom-nav");
+const menuHideBtn = document.querySelector(".menu-hide-btn");
+const menuShowBtn = document.querySelector(".menu-show-btn");
+var navTimeout;
+
+window.addEventListener("scroll", () => {
+  bottomNav.classList.add("active");
+  menuShowBtn.classList.remove("active");
+
+  if (window.scrollY < 10) {
+    menuHideBtn.classList.remove("active");
+    function scrollStopped() {
+      bottomNav.classList.add("active");
+    }
+  }
+  clearTimeout(navTimeout);
+  navTimeout = setTimeout(scrollStopped, 2500);
+
+  if (window.scrollY > 10) {
+    menuHideBtn.classList.add("active");
+    function scrollStopped() {
+      bottomNav.classList.remove("active");
+      menuShowBtn.classList.add("active");
+    }
+  }
+
+  clearTimeout(navTimeout);
+  navTimeout = setTimeout(scrollStopped, 2500);
+});
 
 // Hide bottom navigation menu on click menu-hide-btn.
+menuHideBtn.addEventListener("click", () => {
+  bottomNav.classList.toggle("active");
+  menuHideBtn.classList.toggle("active");
+  menuShowBtn.classList.toggle("active");
+});
 
 // Show bottom navigation menu on click menu-show-btn.
+menuShowBtn.addEventListener("click", () => {
+  bottomNav.classList.toggle("active");
+  menuHideBtn.classList.add("active");
+  menuShowBtn.classList.toggle("active");
+});
 
 /* =====================================================
    To-top-button with scroll indicator bar
 ===================================================== */
 
+window.addEventListener("scroll", () => {
+  const toTopBtn = document.querySelector(".to-top-btn");
+  toTopBtn.classList.toggle("active", window.scrollY > 0);
+
+  //scroll indicator bar
+  const scrollIndicator = document.querySelector(".scroll-indicator-bar");
+
+  const pageScroll =
+    document.body.scrollTop || document.documentElement.scrollTop;
+  const height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+
+  const scrollValue = (pageScroll / height) * 100;
+
+  scrollIndicator.style.height = scrollValue + "%";
+});
+
 /* =====================================================
    Customized cursor on mousemove
 ===================================================== */
 
+const cursor = document.querySelector(".cursor");
+const cursorDot = cursor.querySelector(".cursor-dot");
+const cursorCircle = cursor.querySelector(".cursor-circle");
+
+document.addEventListener("mousemove", (e) => {
+  let x = e.clientX;
+  let y = e.clientY;
+
+  cursorDot.style.top = y + "px";
+  cursorDot.style.left = x + "px";
+  cursorCircle.style.top = y + "px";
+  cursorCircle.style.left = x + "px";
+});
+
 // Cursor effects on hover website elements.
+const cursorHoverLinks = document.querySelectorAll(
+  "body a, .theme-btn, .rega-main-btn, .portfolio-card, .swiper-button-next, .swiper-button-prev, .swiper-pagination-bullet, .service-card, .contact-social-links li, .contact-form .submit-btn, .menu-show-btn, .menu-hide-btn"
+);
+
+cursorHoverLinks.forEach((cursorHoverLink) => {
+  cursorHoverLink.addEventListener("mouseover", () => {
+    cursorDot.classList.add("large");
+    cursorCircle.style.display = "none";
+  });
+});
+
+cursorHoverLinks.forEach((cursorHoverLink) => {
+  cursorHoverLink.addEventListener("mouseout", () => {
+    cursorDot.classList.remove("large");
+    cursorCircle.style.display = "block";
+  });
+});
 
 /* =====================================================
    Website dark/light theme
